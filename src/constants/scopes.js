73ca51503d884057b00c5b8dda57a694e3ad77b2
Rocky Li <rocky.li@sandewisdom.com>
2019-08-02 12:28:41 +0800

    added scopes

---
+++ src/constants/scopes.js
@@ -0,0 +1,10 @@
+const scopes = [
+  'basic',
+  'administrator',
+  'r_user',
+  'w_user',
+  'r_client',
+  'w_client'
+]
+
+module.exports = scopes
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const scopes = require('./constants/scopes')
+
+module.exports = {
+  scopes 
+}
