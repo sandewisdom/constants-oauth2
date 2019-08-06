15b1770028715cceb0d97e61db442e0e270774f9
Rocky Li <rocky.li@sandewisdom.com>
2019-08-06 15:18:06 +0800

    added federation, organization, disabled, and volunteer scopes

--- src/constants/scopes.js
+++ src/constants/scopes.js
@@ -1,10 +1,18 @@
 const scopes = [
-  { name: 'basic' }, 
-  { name: 'administrator' }, 
-  { name: 'r_user' }, 
-  { name: 'w_user' }, 
-  { name: 'r_client' }, 
-  { name: 'w_client' }
+  { name: 'basic' },
+  { name: 'administrator' },
+  { name: 'r_user' },
+  { name: 'w_user' },
+  { name: 'r_client' },
+  { name: 'w_client' },
+  { name: 'r_federation' },
+  { name: 'w_federation' },
+  { name: 'r_organization' },
+  { name: 'w_organization' },
+  { name: 'r_disabled' },
+  { name: 'w_disabled' },
+  { name: 'r_volunteer' },
+  { name: 'w_volunteer' }
 ]
 
 module.exports = scopes
